ca19712a3894522576d028eb4fab75671288df40
Venkat <venkat@codinggeek.com>
2019-05-07 18:50:11 +0530

    boilerplate setup

---
+++ boilerplate/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css'
+import './styles/style.scss'
+
+ReactDOM.render(
+  <p>ReactJS!</p>,
+  document.getElementById('app')
+)
---
+++ boilerplate/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        loader: ['style-loader','css-loader','sass-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
