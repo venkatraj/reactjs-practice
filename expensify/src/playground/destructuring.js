f296da62e06a7496a6c4876fca81f602ee0be74c
Venkat <venkat@codinggeek.com>
2019-05-08 16:05:19 +0530

    array destructuring

--- expensify/src/playground/destructuring.js
+++ expensify/src/playground/destructuring.js
@@ -12,3 +12,23 @@ console.log(`${firstName} is ${age} years old.`)
 
 const { city, temp: temparature } = person.location
 console.log(`It's ${temparature} in ${city}`)
+
+const book = {
+  title: 'The Obstacle is the Way',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
+  }
+}
+
+const { name: publisherName = 'Self-Published' } = book.publisher
+console.log(publisherName)
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+
+const [, myCity, state] = address
+console.log(`You are in ${myCity}, ${state}`)
+
+const item = ['Coffee (ice)', '$2.00', '$2.50', '$2.75']
+const [name, ,medium] = item
+console.log(`A ${name} costs ${medium}`)
