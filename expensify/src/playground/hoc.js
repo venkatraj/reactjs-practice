8ae829eb5add28207c8c6110468b875069fb9234
Venkat <venkat@codinggeek.com>
2019-05-09 21:02:56 +0530

    higher order components

---
+++ expensify/src/playground/hoc.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>This is info: {props.info}</p>
+  </div>
+)
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAdmin && <p>This is private details. Please do not share!</p>}
+      <WrappedComponent {...props} />
+    </div>
+  )
+}
+const AdminInfo = withAdminWarning(Info)
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenticated ? <WrappedComponent {...props} /> : <p>Please login to see info</p>}
+    </div>
+  )
+}
+
+const AuthInfo = requireAuthentication(Info)
+
+// ReactDOM.render(
+//   <AdminInfo isAdmin={true} info="These are the details" />,
+//   document.getElementById('app')
+// )
+
+ReactDOM.render(
+  <AuthInfo isAuthenticated={false} info="These are the details" />,
+  document.getElementById('app')
+)
