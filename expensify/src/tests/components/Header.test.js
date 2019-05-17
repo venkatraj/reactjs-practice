c0bffa60cf91ee105fac150a06389832bde55eea
Venkat <venkat@codinggeek.com>
2019-05-17 19:11:49 +0530

    snapshot testing with enzyme

--- expensify/src/tests/components/Header.test.js
+++ expensify/src/tests/components/Header.test.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import ShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
 import Header from '../../components/Header'
 
 
 test('Should render header correctly', () => {
-  const renderer = new ShallowRenderer()
-  renderer.render(<Header />)
-  expect(renderer.getRenderOutput()).toMatchSnapshot()
+  const wrapper = shallow(<Header />)
+  expect(wrapper).toMatchSnapshot()
 })
