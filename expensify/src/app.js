3371b1ef79ced390a9035eedd9d3a63a6bc5837b
Venkat <venkat@codinggeek.com>
2019-05-21 18:12:00 +0530

    removed manual transactions

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -10,13 +10,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-// Transactions
-const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 80000, createdAt: 2000 }))
-const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', amount: 50000, createdAt: -12000 }))
-const expenseThree = store.dispatch(addExpense({ description: 'To be delete' }))
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 72500, note: 'HP!!' }))
-store.dispatch(removeExpense(expenseThree.expense.id))
-
 ReactDOM.render(
   <Provider store={store}>
     <AppRouter />
--- expensify/src/tests/components/ExpenseForm.test.js
+++ expensify/src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,57 @@ test('Should render ExpenseForm correactly with existing expense', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[0]} />)
   expect(wrapper).toMatchSnapshot()
 })
+
+test('Should render error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  })
+  expect(wrapper.state('error').length).toBeGreaterThan(0)
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('Should set description state correctly', () => {
+  const value = 'Some description'
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.find('input').at(0).simulate('change', {
+    target: { value }
+  })
+  expect(wrapper.state('description')).toBe(value)
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('Should set amount state correctly', () => {
+  const value = '12.34'
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.find('input').at(1).simulate('change', {
+    target: { value }
+  })
+  expect(wrapper.state('amount')).toBe(value)
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('Should not set amount state correctly for invalid amount', () => {
+  const value = '..34'
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.find('input').at(1).simulate('change', {
+    target: { value }
+  })
+  expect(wrapper.state('amount')).toBe('')
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('Should set note state correctly', () => {
+  const value = 'A note to review later'
+  const wrapper = shallow(<ExpenseForm />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.find('textarea').at(0).simulate('change', {
+    target: { value }
+  })
+  expect(wrapper.state('note')).toBe(value)
+  expect(wrapper).toMatchSnapshot()
+})
